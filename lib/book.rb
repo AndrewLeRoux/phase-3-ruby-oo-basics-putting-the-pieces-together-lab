a99b8cdb954c7628ab0a8799c0e605d854be7edc
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-03-09 10:25:12 -0800

    commit tree acbcce717d089031564a76325e4a123f5e280225

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
 
+book = Book.new("Hello")
+book.page_count = 3
+book.turn_page
+puts book.page_count
